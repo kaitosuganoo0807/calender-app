420f0c5ea329e904e8920f0294a1ab40069aec55
kaitosuganoo0807 <k.0807roadster@gmail.com>
2020-03-29 16:46:26 +0900

    リファクタリング

--- front/src/components/CalendarBoard/index.jsx
+++ front/src/components/CalendarBoard/index.jsx
@@ -6,6 +6,8 @@ import * as styles from "./style.css";
 import dayjs from "dayjs";
 import "dayjs/locale/ja";
 
+import CalendarElement from "../CalendarElement";
+
 dayjs.locale("ja");
 
 const createCalendar = () => {
@@ -32,7 +34,7 @@ const CalendarBoard = () => {
       <GridList className={styles.grid} cols={7} spacing={0} cellHeight="auto">
         {calendar.map(c => (
           <li key={c.toISOString()}>
-            <div className={styles.element}>{c.format("D")}</div>
+            <CalendarElement>{c.format("D")}</CalendarElement>
           </li>
         ))}
       </GridList>
