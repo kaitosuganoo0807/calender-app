ee343fb3f7d3ab01109fbaf2849b68d4791b5221
kaitosuganoo0807 <k.0807roadster@gmail.com>
2020-03-29 16:54:08 +0900

    リファクタリング２

--- front/src/components/CalendarBoard/index.jsx
+++ front/src/components/CalendarBoard/index.jsx
@@ -1,31 +1,12 @@
 import React from "react";
 
-import { GridList, StylesProvider } from "@material-ui/core";
+import { GridList } from "@material-ui/core";
 import * as styles from "./style.css";
 
-import dayjs from "dayjs";
-import "dayjs/locale/ja";
-
 import CalendarElement from "../CalendarElement";
-
-dayjs.locale("ja");
-
-const createCalendar = () => {
-  const firstDay = dayjs().startOf("month");
-  const firstDayIndex = firstDay.day();
-  return Array(35)
-    .fill(0)
-    .map((_, i) => {
-      const diffFromFirstDay = i - firstDayIndex;
-      const day = firstDay.add(diffFromFirstDay, "day");
-
-      return day;
-    });
-};
+import { createCalendar } from "../../services/calendar";
 
 const calendar = createCalendar();
-
-
 
 const CalendarBoard = () => {
   console.log(calendar);
